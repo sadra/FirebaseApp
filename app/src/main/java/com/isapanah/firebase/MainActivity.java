e9659829f1e4b38eb07260efdf9430ae956b8150
sadra amlashi <amlashi.sadra@gmail.com>
2017-10-22 15:38:29 +0330

    init

--- app/src/main/java/com/isapanah/firebase/MainActivity.java
+++ app/src/main/java/com/isapanah/firebase/MainActivity.java
@@ -1,13 +1,131 @@
 package com.isapanah.firebase;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import static com.isapanah.firebase.Attribuites.FCM;
+import static com.isapanah.firebase.Attribuites.FCM_ACTION_NEW_NOTIFICATION;
+import static com.isapanah.firebase.Attribuites.FCM_Registered;
+import static com.isapanah.firebase.Attribuites.FCM_TOPIC_ONSCREEN;
+import static com.isapanah.firebase.Attribuites.FCM_TOPIC_VIP_USER;
+import static com.isapanah.firebase.Attribuites.FCM_Token;
+import static com.isapanah.firebase.Attribuites.VIP_USER;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton VIPUser;
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prepareFloatActionButton();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(FCM, 0);
+        if(pref.contains(FCM_Token)){
+            Log.e("FirebaseToken","refID: "+pref.getString(FCM_Token, "null"));
+        }
+
+        RegisterBroadcastService();
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver, new IntentFilter(FCM_ACTION_NEW_NOTIFICATION));
+
+
+        FirebaseMessaging.getInstance().subscribeToTopic(FCM_TOPIC_ONSCREEN);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        FirebaseMessaging.getInstance().unsubscribeFromTopic(FCM_TOPIC_ONSCREEN);
+    }
+
+    private void prepareFloatActionButton(){
+
+        VIPUser = (FloatingActionButton) findViewById(R.id.VIPUserAction);
+        VIPUser.setImageResource( getUserVIP() ? R.drawable.special : R.drawable.not_special );
+
+        VIPUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setUserVIP(!getUserVIP(), view);
+            }
+        });
+
+    }
+
+    private boolean getUserVIP(){
+        return getSharedPreferences(FCM, 0).getBoolean(VIP_USER, false);
+    }
+
+    private void setUserVIP(boolean isVIP, View view){
+
+        getSharedPreferences(FCM, 0).edit().putBoolean(VIP_USER, isVIP).apply();
+        VIPUser.setImageResource( isVIP ? R.drawable.special : R.drawable.not_special );
+
+        if(isVIP){
+            Snackbar.make(view, "You are a VIP user.", Snackbar.LENGTH_LONG).show();
+            FirebaseMessaging.getInstance().subscribeToTopic(FCM_TOPIC_VIP_USER);
+        }else{
+            Snackbar.make(view, "You are a regular user", Snackbar.LENGTH_LONG).show();
+            FirebaseMessaging.getInstance().unsubscribeFromTopic(FCM_TOPIC_VIP_USER);
+        }
+
+    }
+
+    //Firebase Notification broadcast reciever
+    private void RegisterBroadcastService(){
+
+
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (intent.getAction()) {
+                    case FCM_ACTION_NEW_NOTIFICATION:
+                        showDialog(intent);
+                        break;
+                }
+            }
+        };
+
+    }
+
+    private void showDialog(Intent intent){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(intent.getStringExtra("title"));
+        builder.setMessage(intent.getStringExtra("message"));
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+
+    }
+
+
 }
--- app/src/main/java/com/isapanah/firebase/PushNotification/FCMService.java
+++ app/src/main/java/com/isapanah/firebase/PushNotification/FCMService.java
@@ -1,8 +1,168 @@
 package com.isapanah.firebase.PushNotification;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.isapanah.firebase.MainActivity;
+import com.isapanah.firebase.R;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import static com.isapanah.firebase.Attribuites.FCM_ACTION_CLICK_NOTIFICATION;
+import static com.isapanah.firebase.Attribuites.FCM_ACTION_NEW_NOTIFICATION;
+
 /**
  * Created by sadra on 10/21/17.
  */
 
-public class FCMService {
+public class FCMService extends FirebaseMessagingService {
+
+    private static final String TAG = FCMService.class.getSimpleName();
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.e(TAG, "New notification from: " + remoteMessage.getFrom());
+
+        if (remoteMessage == null)
+            return;
+
+        if (remoteMessage.getNotification() != null) {
+            Log.e(TAG, "Notification message: " + remoteMessage.getNotification());
+            showNotificationWithoutData(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }
+
+        if (remoteMessage.getData().size() > 0) {
+            Log.e(TAG, "Data Payload: " + remoteMessage.getData());
+            showNotificationWithData(remoteMessage.getData().get("body"));
+        }
+
+    }
+
+
+    private void showNotificationWithoutData(String title, String body){
+
+        Intent intent = new Intent(this, MainActivity.class); //Open activity if clicked on notification
+        PendingIntent pendingIntent;
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.setAction(FCM_ACTION_CLICK_NOTIFICATION);
+        pendingIntent = PendingIntent.getActivity(this, 0, intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long[] pattern = {500,500,500,500,500}; //Notification vibration pattern
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION); //Notification alert sound
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setVibrate(pattern)
+                .setLights(Color.BLUE,1,1)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(getNotificationIcon())
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setColor(ContextCompat.getColor(this, R.color.colorPrimary));
+
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(getNotify(), notificationBuilder.build());
+
+    }
+
+    private void showNotificationWithData(String body) {
+
+        try{
+
+            JSONObject bodyObjects = new JSONObject(body);
+            if(bodyObjects.getString("type").equals("banner")){
+                showNotificationWithBanner(bodyObjects.getString("title"), bodyObjects.getString("message"), bodyObjects.getString("banner_url"));
+            }else if(bodyObjects.getString("type").equals("dialog_message")){
+                broadcastTheNotification(bodyObjects.getString("title"), bodyObjects.getString("message"));
+            }
+
+        }catch (Exception e){
+            Log.e("There is a problem","Exception: "+e);
+        }
+
+    }
+
+    private void showNotificationWithBanner(String title, String message, String bannerURL){
+
+        Intent intent = new Intent(this, MainActivity.class); //Open activity if clicked on notification
+        PendingIntent pendingIntent;
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.setAction(FCM_ACTION_CLICK_NOTIFICATION);
+        pendingIntent = PendingIntent.getActivity(this, 0, intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long[] pattern = {500,500,500,500,500}; //Notification vibration pattern
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION); //Notification alert sound
+
+        //Style for showing notification With Banner
+        Bitmap remote_picture = BitmapFactory.decodeResource(getResources(), R.drawable.gray_banner);
+        NotificationCompat.BigPictureStyle notiStyle = new NotificationCompat.BigPictureStyle();
+        try {
+            remote_picture = BitmapFactory.decodeStream((InputStream) new URL(bannerURL).getContent());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        notiStyle.bigPicture(remote_picture);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setVibrate(pattern)
+                .setLights(Color.BLUE,1,1)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(getNotificationIcon())
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setColor(ContextCompat.getColor(this, R.color.colorPrimary))
+                .setStyle(notiStyle);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(getNotify(), notificationBuilder.build());
+    }
+
+    private void broadcastTheNotification(String title, String message){
+
+        Intent notification = new Intent(FCM_ACTION_NEW_NOTIFICATION);
+        notification.putExtra("title", title);
+        notification.putExtra("message", message);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(notification);
+    }
+
+    //Random and unique notification ID
+    public int getNotify(){
+        return Integer.parseInt(new SimpleDateFormat("ddHHmmss",  Locale.US).format(new Date()));
+    }
+
+    //In LOLLIPOP and later the small icon has white tint
+    private int getNotificationIcon() {
+        boolean useWhiteIcon = (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP);
+        return useWhiteIcon ? R.mipmap.ic_launcher : R.mipmap.ic_launcher_round;
+    }
+
 }
