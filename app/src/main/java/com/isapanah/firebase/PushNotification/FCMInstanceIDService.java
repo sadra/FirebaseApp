d48d9233005adea856d5b81883f7c568834af650
sadra amlashi <amlashi.sadra@gmail.com>
2017-10-22 15:42:17 +0330

    modify FCMInstanceIDService

--- app/src/main/java/com/isapanah/firebase/PushNotification/FCMInstanceIDService.java
+++ app/src/main/java/com/isapanah/firebase/PushNotification/FCMInstanceIDService.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
-import static com.google.android.gms.internal.zzt.TAG;
 import static com.isapanah.firebase.Attribuites.FCM;
 import static com.isapanah.firebase.Attribuites.FCM_Registered;
 import static com.isapanah.firebase.Attribuites.FCM_Token;
